51b0dfc977da66b6a96566d3b5f1e952fcc1d60d
Nikita Romanenko <romanenko15091999@icloud.com>
2021-10-30 20:38:54 +0300

    Init

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1e725e16fbffb244bb91";
+/******/ 	var hotCurrentHash = "55c732ede8d91196d11c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
